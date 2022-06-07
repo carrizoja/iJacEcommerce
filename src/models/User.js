465919cc080d38e4c109fb13b53c69ae615d55e7
carrizoja <carrizoja@gmail.com>
2022-06-07 17:32:34 -0300

    Tercera Entrega

--- src/models/User.js
+++ src/models/User.js
@@ -16,6 +16,24 @@ const userSchema = new Schema({
         type: String,
         required: true,
     },
+    name: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String,
+        required: true,
+    },
+    age: {
+        type: Number,
+        required: true
+    },
+    avatar: {
+        type: String
+    },
+    admin: {
+        type: Boolean,
+    }
 });
 
 module.exports = mongoose.model('User', userSchema);
